c820cfd238fbdfd68d71aff62bf3dc1dcd23d010
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2024-01-03 23:04:45 +0300

    metin_oku.py fonksiyonlar koyulduç

--- Client/src_text/metin_oku.py
+++ Client/src_text/metin_oku.py
@@ -1,1 +1,74 @@
+#Doktor tarafından gelen mesajların sesli okunması için yapılmış koddur.
+#Emirhan Said ERDEM
+
+import threading
+from responsive_voice import ResponsiveVoice
+
+
+def read_woman(metin):
+        try:
+                engine = ResponsiveVoice()
+                engine = ResponsiveVoice(lang=ResponsiveVoice.TURKISH)
+                engine.say(metin, gender=ResponsiveVoice.FEMALE, rate=0.47, pitch=0.5, vol=1)
+ 
+        except:
+                return
+def read_text__woman_thread(metin):
+       
+        threading.Thread(target=read_woman, args=(metin,)).start()
+
+
+
+def read_man_thread(metin):
+
+            threading.Thread(target=read_man, args=(metin,)).start()
+
+def read_man(metin):
+        try:
+                engine = ResponsiveVoice()
+                engine = ResponsiveVoice(lang=ResponsiveVoice.TURKISH)
+                engine.say(metin, gender=ResponsiveVoice.MALE, rate=0.47, pitch=0.36, vol=1)
+        except:
+                return
+
+def read_old_man(metin):
+        try:
+                engine = ResponsiveVoice()
+                engine = ResponsiveVoice(lang=ResponsiveVoice.TURKISH)
+                engine.say(metin, gender=ResponsiveVoice.MALE, rate=0.33, pitch=0.25, vol=1)
+        except:
+                return       
+
+def read__old_man_t(metin):
+
+        threading.Thread(target=read_old_man, args=(metin,)).start()
+
+
+def read_old_woman(metin):
+        try:
+                engine = ResponsiveVoice()
+                engine = ResponsiveVoice(lang=ResponsiveVoice.TURKISH)
+                
+                engine.say(metin, gender=ResponsiveVoice.FEMALE, rate=0.36, pitch=0.28, vol=1)
+        except:
+                return
+       
+
+def read__old_woman_t(metin):
+
+        threading.Thread(target=read_old_woman, args=(metin,)).start()
+
+def read_children(metin):
+        try:
+
+                engine = ResponsiveVoice()
+                engine = ResponsiveVoice(lang=ResponsiveVoice.TURKISH)
+                engine.say(metin, gender=ResponsiveVoice.FEMALE, rate=0.45, pitch=0.75, vol=0.5)
+        except:
+                return
+        
+def read_children_thread(metin):
+
+        threading.Thread(target=read_children, args=(metin,)).start()
+
 
