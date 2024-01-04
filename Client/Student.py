5e5a0c420eb7c276bd6aedac79ff26de838e441a
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2024-01-04 18:49:19 +0300

    Student'e Mesaj gönderme eklendi.
    tek taraflı ses gönderimde sorun yok ancak çift taraflı ses gönderiminde hata var.
    start_commination pasif hale getirildi

--- Client/Student.py
+++ Client/Student.py
@@ -1,7 +1,10 @@
-#sesli iletişim uygulamasının arayüzsüz hali için yapıldı.
-#Rise Together
+# sesli iletişim uygulamasının arayüzsüz hali için yapıldı.
+# Rise Together
+# Emirhan Said ERDEM
 
-#Emirhan Said ERDEM
+# İlk olarak enter_room fonksiyonundaki ip adresinizi güncelleyin ve kodu çalıştırın
+# Kullanıcı adınızı ardından Doktor tarafının oluşturduğu odanın kodunu girin artık iletişime başlayabilirsiniz.
+
 
 import threading
 import pyaudio
@@ -12,10 +15,13 @@ from cryptography.fernet import Fernet
 import base64
 import os
 import time
+import keyboard
+from src_text.metin_oku import *
+
 sio = socketio.Client()
 
 
-class istemci_page():
+class istemci_page:
     def __init__(self) -> None:
         super().__init__()
         # ses iletiminde sesin özellikleri:
@@ -29,70 +35,35 @@ class istemci_page():
 
         self.Event = threading.Event()
         self.oda_kodu = ""
-        self.set_output_stream()# hoparlör bağlantısı en başta yapılıyor.
+        self.set_output_stream()  # hoparlör bağlantısı en başta yapılıyor.
         self.kullanici_ad = input("kullanici adini giriniz:\n")
-        self.oda_kodu_ID()
         
-        self.start_communication()
-    def oda_kodu_ID(self):
 
-        """
-        MySQL veritabanına bağlantı oluşturur.
-        """
-        ID = self.kullanici_ad
-        print(ID)
+        self.enter_room()
+
+    def enter_room(self,):  # flask ile kurulan odaya giriş yapılıyor. Ses ve metin alışverişi başlatılıyor
         try:
-                connection = mysql.connector.connect(
-                host="rise.czfoe4l74xhi.eu-central-1.rds.amazonaws.com",
-                user="admin",
-                password="Osmaniye12!",
-                database="rise_data")
-                cursor = connection.cursor()
-                query = "SELECT oda_kodu FROM veriler WHERE kullanici_ad = %s"
-                cursor.execute(query, (ID,))
-                kullanici_verileri = cursor.fetchone()
+            room = input("Lütfen Girmek istediğiniz odanın Kodunu yazınız:")
 
-                self.oda_kodu = kullanici_verileri[0]
-                print(self.oda_kodu)
+            name = self.kullanici_ad
 
-                connection.commit()
-                connection.close()
+            self.receive_text()
+            self.yazi_gonder_t()
+            # self.start_communication()
+            sio.on("data1", self.get_sound)
 
-                self.enter_room()
+            @sio.event
+            def connect():  # flaska adımız ve oda bilgisi gönderiliyor.
+                sio.emit("baglan", {"name": name, "room": room})
 
-        except Exception as e:
-            print("mysql baglanti hata",e)
-        
-
-    def enter_room(self):  # flask ile kurulan odaya giriş yapılıyor. Ses ve metin alışverişi başlatılıyor
-        room = self.oda_kodu
-        print(self.oda_kodu)
-        name = "öğrenci"
-
-        self.receive_text()
-
-        @sio.event
-        def connect():
-            print("Connected to server")
-            sio.emit('baglan', {"name": name, "room": room})
-
-        # self.send_output_device_list()
-        #sio.on("index", self.select_output_device)
-
-        sio.on("data1", self.get_sound)
-
-        sio.connect("http://192.168.1.45:5000", auth={"name": name, "room": room})
-        sio.wait()
-
-
+            sio.connect("http://YOUR_IP_ADRESS:5000", auth={"name": name, "room": room})
+            # sio.wait()
+        except:
+            print("Yanlış oda Kodu girmiş olabilirsiniz Lütfen tekrar yazınız.")
 
     def receive_text(self):
-
-        @sio.on(
-            "message"
-        )  # flask projesindeki message olayına 'on' ile bağlanıyoruz. bu şekilde mesaj gönderildiğinde handle_message aktif olacak
+        @sio.on("message")  # flask projesindeki message olayına 'on' ile bağlanıyoruz. bu şekilde mesaj gönderildiğinde handle_message aktif olacak
         def handle_message(message):
-            
             if "message" in message:
                 text = message.get("message", "")  # Şifreli mesaj içeriğini al
                 efekt = message.get("efekt", "")  # Efekti al
@@ -102,73 +73,70 @@ class istemci_page():
                     pass
                 else:
                     # Mesajı çöz
-                    
+
                     key = base64.urlsafe_b64decode(key.decode("utf-8"))  # Anahtarı çöz
                     decrypted_message = self.decrypt_message(text, key)
 
                     print(decrypted_message)
-                    #self.istemci.metin_yeri.insertPlainText(f"Mesaj: {decrypted_message}\n")  # Çözülmüş mesajı pyqt5 alanına ekle ve efekte göre okut
+
                     if efekt == 0:
-                        #read_man(decrypted_message)
-                        print(decrypted_message)
+                        read_man(decrypted_message)
+
                     elif efekt == 1:
-                        #read_text__woman_thread(decrypted_message)
-                        print(decrypted_message)
+                        read_text__woman_thread(decrypted_message)
+
                     elif efekt == 2:
-                        #read_children(decrypted_message)
-                        print(decrypted_message)
+                        read_children(decrypted_message)
+
                     elif efekt == 3:
-                        #read_old_woman(decrypted_message)
-                        print(decrypted_message)
+                        read_old_woman(decrypted_message)
+
                     elif efekt == 4:
-                        #read_old_man(decrypted_message)
-                        print(decrypted_message)
+                        read_old_man(decrypted_message)
+
             else:
                 print(message)
                 pass
 
     def decrypt_message(
-            self, encrypted_message, key
+        self, encrypted_message, key
     ):  # gelen şifreli metni ve keyi alıyoruz.
         cipher_suite = Fernet(base64.urlsafe_b64encode(key).decode("utf-8"))
         decrypted_message = cipher_suite.decrypt(
             encrypted_message
         ).decode()  # şifreleri çözüp normal asıl metnimize ulaşıyoruz
         return decrypted_message
-    
+
     def get_sound(self, data):
         try:
-            if  data:
+            if data:
                 audio_data = data.get("audio_data", b"")
-                self.stream.write(audio_data)
+                self.output_stream.write(audio_data)
             elif not data:
                 print("Data yok")
-            
+
         except Exception as e:
             print("Ses alma hatası:", str(e))
 
-
-
     def send_audio(self):
+        print("a2")
         p = pyaudio.PyAudio()
         stream = p.open(
             format=pyaudio.paInt16,
             channels=self.CHANNELS,
-            rate=44100,
+            rate=self.RATE,
             input=True,
             frames_per_buffer=self.CHUNK,
         )
 
         try:
-
             while True:
                 data = stream.read(self.CHUNK)
                 audio_data = np.frombuffer(data, dtype=np.int16)
 
                 audio_data = audio_data.tobytes()
-
-                sio.emit("audio_data2", {
-                    "audio_data2": audio_data})  # Bytlara dönüştürülen ses verilerini 'audio_data' sözcüğü ile emitle emitle
+                # stream.write(audio_data)
+                sio.emit("audio_data2", {"audio_data2": audio_data})  # Bytlara dönüştürülen ses verilerini 'audio_data' sözcüğü ile emitle emitle
         except Exception as e:
             print("hata", e)
         finally:
@@ -179,31 +147,62 @@ class istemci_page():
     def start_communication(self):
         threading.Thread(target=self.send_audio).start()
 
-    
+    def yazi_gonder(self):
+        room = self.oda_kodu
 
-    def set_output_stream(self):
-       
-        if self.stream is None:
-            
+        name = self.kullanici_ad
+        while True:
+            try:
+                message = input("Göndereceğiniz Metin girin: ")
+
+                # Anahtar oluştur
+                key = self.generate_key()  # text için key oluştur
+
+                encrypted_message = self.encrypt_message(message, key)  # Mesajı şifrele
+
+                # flaskta ki message olayına şifreli mesajı- room-efekt ve keyi sözcük içinde emitle
+                sio.emit(
+                    "message_student",
+                    {
+                        "data": encrypted_message.decode(),
+                        "room": room,
+                        "name": name,
+                        "key": key,
+                    },
+                )
+            except KeyboardInterrupt:
+                pass
+
+    def yazi_gonder_t(self):
+        t1 = threading.Thread(target=self.yazi_gonder)
+        t1.start()
+
+    def generate_key(self):
+        return Fernet.generate_key()  # key oluştur
+
+    def encrypt_message(self, message, key):  # keyi ve mesajı al
+        cipher_suite = Fernet(key)  # şifreli paketi oluştur
+        encrypted_message = cipher_suite.encrypt(message.encode())
+        return encrypted_message  # şifrelenmiş mesajı oluştur ve return et
+
+    def set_output_stream(self):  # hoaprlörü ayarlar
+        if self.output_stream is None:
             p = pyaudio.PyAudio()
             try:
-                self.stream = p.open(
+                self.output_stream = p.open(
                     output=True,
                     format=pyaudio.paInt16,
                     channels=self.CHANNELS,
                     rate=self.RATE,
                     frames_per_buffer=self.CHUNK,
                 )
-               
+
             except OSError as e:
                 print("Hoparlör bağlantısı yapılamadı:", e)
-                self.stream = None
+                self.output_stream = None
         else:
             print("dolu")
 
-    
 
 if __name__ == "__main__":
-        app = istemci_page()
-        while True:
-            pass  # Programın kapanmaması için sonsuz döngü
+    app = istemci_page()
